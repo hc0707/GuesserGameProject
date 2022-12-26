bcc452cba24e43d77629b45c5bb78d6dc0f5a75b
hc0707 <115972653+hc0707@users.noreply.github.com>
2022-12-26 12:06:20 +0530

    Update Project_GuesserGame.java

--- Project_GuesserGame.java
+++ Project_GuesserGame.java
@@ -5,171 +5,217 @@ import java.util.Scanner;
 // 1--Made all data members private(encapsulation)
 // 2--Created a constructor in Player class to get name of player as argument 
 //    when Player object is created and to print the name when taking input
-// 3--added methods both in player class and umpire class to increment the score 
+// 3--added methods both in player class to increment the score 
 // 	  and also display them.
-// 4--Added functionality to either continue the game or exit it, because of which i 
-//    had to change the compare method and call all other umpire method inside compare method
-//    rather than calling in main method because it will reset the scores to 0 when 
-//    continuing the game.
-
+// 4--Added functionality to either continue the game or exit it
+// 5--Added Draw method to be called if 2 players guessed correctly.
 
 public class Project_GuesserGame {
-    public static void main(String[] args) {
 
-        
-		System.out.println("****Welcome to The Guesser Game****");
-		System.out.println();
-		Umpire u=new Umpire();
-		
-		u.play();
-			
-    }
+	public static void main(String[] args) {
+
+		while (true) {
+			System.out.println("****Welcome to The Guesser Game****");
+			System.out.println();
+			Umpire u = new Umpire();
+			u.collectNumFromGuesser();
+			u.collectNumFromPlayers();
+			u.play();
+
+			System.out.println();
+
+			u.scoreBoard();
+
+			System.out.println("Do you want to continue the game(y/n)");
+			Scanner scan = new Scanner(System.in);
+			String s= scan.next();
+			if (s.equals("y"))
+				continue;
+			else if (s.equals("n"))
+				System.out.println("ThankYou for playing!!");
+				break;
+		}
+	}
 }
 
-class Guesser
-{
+class Guesser {
 	private int guessNum;
-	
-	int guessNum()
-	{
-		Scanner scan=new Scanner(System.in);
+
+	int guessNum() {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Guesser kindly guess the number");
-		guessNum=scan.nextInt();
+		guessNum = scan.nextInt();
 		return guessNum;
-	}	
+	}
 }
 
-class Player
-{
+class Player {
 	private int guessNum;
-	private String name;
-	private int score;
+	String name;
+	private static int scoreP1;
+	private static int scoreP2;
+	private static int scoreP3;
 
-  	Player(String name)
-	{ 
-		this.name = name; 
+	Player(String name) {
+		this.name = name;
 	}
-	int guessNum()
-	{
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Player " + name +" kindly guess the number");
-		guessNum=scan.nextInt();
+
+	int guessNum() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Player " + name + " kindly guess the number");
+		guessNum = scan.nextInt();
 		return guessNum;
 	}
-	void dispScore()
-	{
-		System.out.println(name+":"+score);
+
+	static void dispScore() {
+		System.out.println("_______Score Board______");
+		System.out.println("Player1" + ":" + scoreP1);
+		System.out.println("Player2" + ":" + scoreP2);
+		System.out.println("Player3" + ":" + scoreP3);
 	}
-	void updateScore()
-	{
-		score++;
+
+	static void updateScoreP1() {
+		scoreP1++;
+	}
+	static void updateScoreP2() {
+		scoreP2++;
+	}
+	static void updateScoreP3() {
+		scoreP3++;
 	}
 }
 
-class Umpire
-{
+class Umpire {
 	private int numFromGuesser;
 	private int numFromPlayer1;
 	private int numFromPlayer2;
 	private int numFromPlayer3;
-	
-	Player p1=new Player("p1");
-	Player p2=new Player("p2");
-	Player p3=new Player("p3");
-	
-	void collectNumFromGuesser()
-	{
-		Guesser g=new Guesser();
-		numFromGuesser=g.guessNum();
+	private int drawNum1;
+	private int drawNum2;
+
+	Player p1 = new Player("p1");
+	Player p2 = new Player("p2");
+	Player p3 = new Player("p3");
+	Guesser g = new Guesser();
+
+	void collectNumFromGuesser() {
+		numFromGuesser = g.guessNum();
 	}
 
-	void collectNumFromPlayers()
-	{
-		numFromPlayer1=p1.guessNum();
-		numFromPlayer2=p2.guessNum();
-		numFromPlayer3=p3.guessNum();
+	void collectNumFromPlayers() {
+		numFromPlayer1 = p1.guessNum();
+		numFromPlayer2 = p2.guessNum();
+		numFromPlayer3 = p3.guessNum();
 	}
 
-	void scoreBoard()
-	{
-		System.out.println("_______Score Board______");
-		p1.dispScore();
-		p2.dispScore();
-		p3.dispScore();
+	void scoreBoard() {
+		
+		Player.dispScore();
 	}
+	void draw(Player draw1, Player draw2){
+		collectNumFromGuesser();
+		drawNum1=draw1.guessNum();
+		drawNum2=draw2.guessNum();
+	}
+	void play() {
 
-	void play()
-	{
+		if (numFromGuesser == numFromPlayer1) {
+			if (numFromGuesser == numFromPlayer2 && numFromGuesser == numFromPlayer3) {
+				System.out.println("----------------");
+				System.out.println("No one won!Draw between all Players.");
+				System.out.println("----------------");
+			}
 
-		while(true)//to continue the game
-		{
-			this.collectNumFromGuesser();
-			this.collectNumFromPlayers();
-
-			if(numFromGuesser==numFromPlayer1)
-			{
-				if(numFromGuesser==numFromPlayer2 && numFromGuesser==numFromPlayer3)
-				{
-					System.out.println("All players won the game");
-					p1.updateScore();
-					p2.updateScore();
-					p3.updateScore();
-				}
-
-				else if(numFromGuesser==numFromPlayer2 )
-				{
-					System.out.println("Player 1 & Player2 won");
-					p1.updateScore();
-					p2.updateScore();
-				}
-				else if(numFromGuesser==numFromPlayer3)
-				{
-					System.out.println("Player 1 & Player3 won");
-					p1.updateScore();
-					p3.updateScore();
-				}
-				else
-				{
+			else if (numFromGuesser == numFromPlayer2) {
+				System.out.println("----------------");
+				System.out.println("Draw between Player 1 & Player2");
+				System.out.println("----------------");
+				draw(p1, p2);
+				if (drawNum1==numFromGuesser&&drawNum2!=numFromGuesser) {
+					System.out.println("----------------");
 					System.out.println("Player 1 won the game");
-					p1.updateScore();
+					System.out.println("----------------");
+					Player.updateScoreP1();
 				}
-			}
-			else if(numFromGuesser==numFromPlayer2)
-			{
-				if(numFromGuesser==numFromPlayer3)
-				{
-					System.out.println("Player 2 & Player3 won");
-					p2.updateScore();
-					p3.updateScore();
-				}
-				else
-				{
+				else if (drawNum2==numFromGuesser&&drawNum1!=numFromGuesser) {
+					System.out.println("----------------");
 					System.out.println("Player 2 won the game");
-					p2.updateScore();	
+					System.out.println("----------------");
+					Player.updateScoreP2();
 				}
+				else{
+					System.out.println("----------------");
+					System.out.println("Nobody won!");
+					System.out.println("----------------");
+				}
+			} else if (numFromGuesser == numFromPlayer3) {
+				System.out.println("----------------");
+				System.out.println("Draw between Player 1 & Player3");
+				System.out.println("----------------");
+				draw(p1, p3);
+				if (drawNum1==numFromGuesser&&drawNum2!=numFromGuesser) {
+					System.out.println("----------------");
+					System.out.println("Player 1 won the game");
+					System.out.println("----------------");
+					Player.updateScoreP1();
+				}
+				else if (drawNum2==numFromGuesser&&drawNum1!=numFromGuesser) {
+					System.out.println("----------------");
+					System.out.println("Player 3 won the game");
+					System.out.println("----------------");
+					Player.updateScoreP3();
+				}
+				else{
+					System.out.println("----------------");
+					System.out.println("Nobody won!");
+					System.out.println("----------------");
+				}
+			} else {
+				System.out.println("----------------");
+				System.out.println("Player 1 won the game");
+				System.out.println("----------------");
+				Player.updateScoreP1();
 			}
-			else if(numFromGuesser==numFromPlayer3)
-			{
-				System.out.println("Player 3 won the game");
-				p3.updateScore();
+		} else if (numFromGuesser == numFromPlayer2) {
+			if (numFromGuesser == numFromPlayer3) {
+				System.out.println("----------------");
+				System.out.println("Draw between Player 2 & Player3");
+				System.out.println("----------------");
+				draw(p2, p3);
+				if (drawNum1==numFromGuesser&&drawNum2!=numFromGuesser) {
+					System.out.println("----------------");
+					System.out.println("Player 2 won the game");
+					System.out.println("----------------");
+					Player.updateScoreP2();
+				}
+				else if (drawNum2==numFromGuesser&&drawNum1!=numFromGuesser) {
+					System.out.println("----------------");
+					System.out.println("Player 3 won the game");
+					System.out.println("----------------");
+					Player.updateScoreP3();
+				}
+				else{
+					System.out.println("----------------");
+					System.out.println("Nobody won!");
+					System.out.println("----------------");
+				}
+			} else {
+				System.out.println("----------------");
+				System.out.println("Player 2 won the game");
+				System.out.println("----------------");
+				Player.updateScoreP2();
 			}
-			else
-			{
-				System.out.println("Game lost Try Again!");
-			}
-			
-			System.out.println();
+		} else if (numFromGuesser == numFromPlayer3) {
+			System.out.println("----------------");
+			System.out.println("Player 3 won the game");
+			System.out.println("----------------");
+			Player.updateScoreP3();
+		} else {
+			System.out.println("----------------");
+			System.out.println("Nobody won!");
+			System.out.println("----------------");
+		}
 
-			this.scoreBoard();
-
-			System.out.println("Do you want to continue the game(y/n)");
-			Scanner scan = new Scanner(System.in);
-
-				if (scan.next().equals("y"))
-					continue;
-				else if(scan.next().equals("n"))
-					break;
-		}		
 	}
-	
+
 }
